eb0e5eeb88d3d608eff17999909a5c55eef264df
OFM <mereyomerfaruk@gmail.com>
2026-03-14 22:19:53 +0300

    fix: address

--- src/App.tsx
+++ src/App.tsx
@@ -186,16 +186,6 @@ const Contact = () => (
           <div className="space-y-6">
             <div className="flex items-start gap-4">
               <div className="w-10 h-10 bg-zinc-100 rounded-full flex items-center justify-center shrink-0">
-                <MapPin className="w-5 h-5" />
-              </div>
-              <div>
-                <h4 className="font-bold mb-1">Office Address</h4>
-                <p className="text-zinc-600">NO:5E YASAMKENT MAHALLESI 3035 CADDESI</p>
-                <p className="text-zinc-600 text-sm italic mt-1">Ankara, Turkey</p>
-              </div>
-            </div>
-            <div className="flex items-start gap-4">
-              <div className="w-10 h-10 bg-zinc-100 rounded-full flex items-center justify-center shrink-0">
                 <Mail className="w-5 h-5" />
               </div>
               <div>
